16075ebaf93562dad0dc287e6ceda034f2c38a19
mr-junniepat <patrickoigwe@gmail.com>
2022-10-20 19:38:33 +0400

    changes to code

--- batter/game/scripting/draw_hud_action.py
+++ batter/game/scripting/draw_hud_action.py
@@ -18,9 +18,9 @@ class DrawHudAction(Action):
     # **********************************************************************************************
     # todo: fix the bug by making sure the text value is set to the appropriate variable.
     def _draw_label(self, cast, group, format_str, data):
-        # the_value_to_display = format_str.format(data)
+        the_value_to_display = format_str.format(data)
         label = cast.get_first_actor(group)
         text = label.get_text()
-        text.set_value(format_str.format(data))
+        text.set_value(the_value_to_display)
         position = label.get_position()
         self._video_service.draw_text(text, position)
